086c2666eaf154b6e2dc501be22c1d1523be0c09
Anna Lingoth <pri.anna@gmx.net>
2024-02-17 19:58:29 +0100

    commit

---
+++ src/main/java/lesson09/Beratung.java
@@ -0,0 +1,11 @@
+package lesson09;
+
+public class Beratung {
+    public static void main(String[] args) {
+        char charDigitOne = '5';
+        char charDigitTwo = 'd';
+        boolean isDigitcharDigitOne = Character.isDigit(charDigitOne);
+        boolean isDigitcharDigitTwo = Character.isDigit(charDigitTwo);
+
+    }
+}
--- src/main/java/lesson09/Homework_1.java
+++ src/main/java/lesson09/Homework_1.java
@@ -1,10 +1,60 @@
 package lesson09;
 
 import static java.lang.Math.*;
+import java.util.Scanner;
 
 public class Homework_1 {
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random()*100) + 1;
-        System.out.println(randomNumber);
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber;
+
+        boolean continueGame = true;
+        do {
+            randomNumber = (int) (Math.random() * 100) + 1;
+            int attempts = 0;
+            System.out.println(randomNumber);
+
+            while (continueGame) {
+                System.out.println("Gebe eine Zahl zwischen 1 und 100 ein: ");
+                int number = scanner.nextInt();
+                int i = attempts++;
+
+
+                if (number == randomNumber) {
+                    System.out.println("Glückwunsch! Du hast die Zahl " + randomNumber + " beim " + attempts + ". Versuch erraten!");
+                    System.out.println("Möchtest du das Spiel nochmal spielen: y/n ? ");
+                    String answer;
+                    answer = scanner.next();
+                    if (answer.equalsIgnoreCase("n")) {
+                        System.out.println("Danke fürs Spielen. Ende. ");
+                        continueGame = false;
+                        scanner.close();
+                        continue;
+                    }
+                } else {
+                    continueGame = true;
+                    randomNumber = (int) (Math.random() * 100) + 1;
+                    System.out.println(randomNumber);
+                    int a = attempts++;
+                }
+
+
+                if (number > randomNumber) {
+
+                    System.out.println("Falsch! Deine Zahl ist größer als die gesuchte Zahl! Versuche noch einmal. Gebe eine Zahl ein:  ");
+                    int number1 = scanner.nextInt();
+                } else {
+                    System.out.println("Falsch! Deine Zahl ist kleiner als die gesuchte Zahl! Versuche noch einmal. Gebe eine Zahl ein: ");
+                    int number1 = scanner.nextInt();
+                }
+            }
+        }
+
     }
 }
+
+
+
+
+
+
