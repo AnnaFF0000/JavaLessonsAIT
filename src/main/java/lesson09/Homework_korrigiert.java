a72cd66df69cab25ac81654e7a6a4a68305b6156
Anna Lingoth <pri.anna@gmx.net>
2024-02-17 19:58:10 +0100

    commit

--- src/main/java/lesson09/Homework_korrigiert.java
+++ src/main/java/lesson09/Homework_korrigiert.java
@@ -2,7 +2,7 @@ package lesson09;
 
 import java.util.Scanner;
 
-public class Homework_korregiert {
+public class Homework_korrigiert {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
